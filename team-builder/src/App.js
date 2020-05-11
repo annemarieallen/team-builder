861f6c0a6d16267e40b6c86a1f619eeb40767894
Annemarie Allen <annemarie@Annemaries-MacBook-Pro.local>
2020-05-11 12:23:12 -0800

    added form to screen, not yet fully functional

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
+import Form from './components/Form';
 import './App.css';
 
+
+
+
+
 function App() {
+
+  //Setup state
+  const [teamMembers, setTeamMembers] = useState ({
+    id: Date.now(),
+    name: 'Annemarie',
+    email: 'annemarie@email.com',
+    role: 'student',
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Member Submission</h1>
+      <Form onSubmit = {handleSubmit}/>
+      
     </div>
   );
 }
